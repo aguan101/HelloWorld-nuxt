28da9595478ec3ae4f1304e277a26b056299acfd
Mac <mac@MacdeMacBook-Pro.local>
2022-11-30 11:27:42 +0800

    Core: add version variable

--- app.vue
+++ app.vue
@@ -1,5 +1,16 @@
+<script setup>
+const VERSION = 2 + 1
+</script>
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="hello">
+    Hello Nuxt {{ VERSION }}
   </div>
 </template>
+
+<style scoped>
+.hello {
+  font-family: serif;
+  font-size: 3rem;
+  padding: 10rem;
+}
+</style>
